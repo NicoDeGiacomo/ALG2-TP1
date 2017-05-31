25d009dfd726a7583c24a508151bc1d7838ab0d0
NicoDeGiacomo <nicolas.de.giacomo@gmail.com>
2017-05-30 22:04:58 -0300

    Filesplit 90%fix

--- filesplit.c
+++ filesplit.c
@@ -4,20 +4,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define FILENAME_LEN 6 //'_' + 4 digitos + \0
 
-void filesplit(const char* name, int n);
-
-void filesplit(const char* name, int n){
+int filesplit(const char* name, int n){
     FILE* fileRead = fopen(name, "r");
+    if (!fileRead) {
+        fprintf(stderr, "Error opening the file\n");
+        return 1;
+    }
 
-    int len = 6; //'_' + 4 digitos + \0
-    char buffer[strlen(name) + len];
-    char number[len];
-    snprintf(number, sizeof(char) * len, "_%04d", 1);
+    char buffer[strlen(name) + FILENAME_LEN];
+    char number[FILENAME_LEN];
+    snprintf(number, sizeof(char) * FILENAME_LEN, "_%04d", 1);
     strcpy(buffer, name);
     strcat(buffer, number);
 
     FILE* fileWrite = fopen(buffer, "w");
+    if (!fileWrite) {
+        fprintf(stderr, "Error creating a file\n");
+        return 1;
+    }
     int count = 1;
     int c = fgetc(fileRead);
     while (c != EOF){
@@ -28,25 +34,28 @@ void filesplit(const char* name, int n){
             fclose(fileWrite);
             //No abro un nuevo archivo si el prox char es un EOF (Para que no quede vacio)
             if (c != EOF){
-                snprintf(number, sizeof(char) * len, "_%04d", count/n+1);
+                snprintf(number, sizeof(char) * FILENAME_LEN, "_%04d", count/n+1);
                 strcpy(buffer, name);
                 strcat(buffer, number);
                 fileWrite = fopen(buffer, "w");
+                if (!fileWrite) {
+                    fprintf(stderr, "Error creating a file\n");
+                    return 1;
+                }
             }
         }
         count++;
     }
     fclose(fileWrite);
     fclose(fileRead);
-
+    return 0;
 }
 
 int main(int argc, char const *argv[]){
-    if (argc > 3 || argc < 3 ){
+    if (argc != 3){
         fprintf(stderr, "Please provide 2 arguments\n");
         return 1;
     }
 
-    filesplit(argv[1], atoi(argv[2]));
-    return 0;
+    return filesplit(argv[1], atoi(argv[2]));
 }
