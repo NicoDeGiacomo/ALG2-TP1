f1d88621d3eae1ffcad49628bddb0e4272bab160
Nicolas De Giacomo <nicolas.de.giacomo@gmail.com>
2017-06-15 19:01:29 -0300

    DONE Filesplit

--- filesplit.c
+++ filesplit.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
 #define FILENAME_LEN 6 //'_' + 4 digitos + \0
 
 int filesplit(const char* name, int n){
@@ -13,40 +15,32 @@ int filesplit(const char* name, int n){
         return 1;
     }
 
-    char buffer[strlen(name) + FILENAME_LEN];
-    char number[FILENAME_LEN];
-    snprintf(number, sizeof(char) * FILENAME_LEN, "_%04d", 1);
-    strcpy(buffer, name);
-    strcat(buffer, number);
 
-    FILE* fileWrite = fopen(buffer, "w");
-    if (!fileWrite) {
-        fprintf(stderr, "Error creating a file\n");
-        return 1;
-    }
     int count = 1;
-    int c = fgetc(fileRead);
-    while (c != EOF){
-        fputc(c, fileWrite);
-        c = fgetc(fileRead);
-        if (!(count % n)){
-            fputc('\n', fileWrite);
-            fclose(fileWrite);
-            //No abro un nuevo archivo si el prox char es un EOF (Para que no quede vacio)
-            if (c != EOF){
-                snprintf(number, sizeof(char) * FILENAME_LEN, "_%04d", count/n+1);
-                strcpy(buffer, name);
-                strcat(buffer, number);
-                fileWrite = fopen(buffer, "w");
-                if (!fileWrite) {
-                    fprintf(stderr, "Error creating a file\n");
-                    return 1;
-                }
-            }
+    while (!feof(fileRead)){
+        //Leo una linea de n chars
+        char linea[n];
+        size_t leidos = fread(linea, sizeof(char), (size_t) n, fileRead);
+
+        //Guardo en un archivo
+        char buffer[strlen(name) + FILENAME_LEN];
+        char number[FILENAME_LEN];
+        snprintf(number, sizeof(char) * FILENAME_LEN, "_%04d", count);
+        strcpy(buffer, name);
+        strcat(buffer, number);
+
+        FILE* fileWrite = fopen(buffer, "w");
+        if (!fileWrite) {
+            fclose(fileRead);
+            fprintf(stderr, "Error creating a file\n");
+            return 1;
         }
+        fwrite(linea, sizeof(char), (size_t) leidos, fileWrite);
         count++;
+        fclose(fileWrite);
+
     }
-    fclose(fileWrite);
+
     fclose(fileRead);
     return 0;
 }
